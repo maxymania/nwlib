d1d8f6111681fd3a2bdcd4f0fcd92275dfa48495
Simon Schmidt <simon.schmidt@htp-tel.de>
2016-03-30 09:59:19 +0200

    Fix error!

--- nw_unix.c
+++ nw_unix.c
@@ -30,7 +30,7 @@ scka nw_listen_unix(const char* sckf){
 	struct sockaddr_un serv_addr;
 	serv_addr.sun_family = AF_UNIX;
 	strcpy(serv_addr.sun_path,sckf);
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
 	if(sockfd<0)return (scka){-1,0};
 	if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr))<0){
 		close(sockfd);
@@ -43,7 +43,7 @@ int nw_connect_unix(const char* sckf){
 	struct sockaddr_un serv_addr;
 	serv_addr.sun_family = AF_UNIX;
 	strcpy(serv_addr.sun_path,sckf);
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
 	if(sockfd<0)return -1;
 	if(connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr))<0){
 		close(sockfd);
